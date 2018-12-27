d4bb46c6a759f0176c1aab87cc7968c558a30e95
UgoNic <ugo.nicolai@gmail.com>
2018-12-28 04:13:02 +1100

    design home. stuck on design button

--- components/Home.js
+++ components/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import {Button, TextInput, View, Text} from 'react-native'
+import {Button, TextInput, View, Text, ImageBackground} from 'react-native'
 import {createStackNavigator} from 'react-navigation'
 import { Font, AppLoading } from "expo"
 
+import {home} from './Style'
 
 export default class Home extends React.Component {
     static navigationOptions = {
@@ -16,7 +17,9 @@ export default class Home extends React.Component {
 
     async componentWillMount() {
         await Font.loadAsync({
-          'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf')
+          'Montserrat-Light': require('../assets/fonts/Montserrat-Light.ttf'),
+          'Montserrat-LightItalic': require('../assets/fonts/Montserrat-LightItalic.ttf')
+
         })
         this.setState({ fontLoaded: false });
     }
@@ -33,17 +36,24 @@ export default class Home extends React.Component {
         }
         else
             return (
-                <View>
-                    <Text style={{fontFamily: 'Montserrat-Light', color: 'black', marginTop: 30}}>Weather App</Text>
-                    <TextInput
-                        style={{height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 30}}
-                        placeholder='Enter the name of a city'
-                        placeholderTextColor='black'
-                        onChangeText={(search) => this.setState({search})}
-                        value={this.state.search}
-                        onSubmitEditing={() => this.submit()}
-                    />
-                </View>
+                <ImageBackground source={require('../images/home.jpg')} style={home.image} >
+                    <View style={home.view}>
+                        <Text style={home.title}>Weather App</Text> 
+                        <TextInput
+                            style={[home.input, (this.state.search) ? {fontFamily: 'Montserrat-Light'} : {fontFamily: 'Montserrat-LightItalic'}]}
+                            placeholder='Enter the name of a city'
+                            placeholderTextColor='black'
+                            onChangeText={(search) => this.setState({search})}
+                            value={this.state.search}
+                            onSubmitEditing={() => this.submit()}
+                        />
+                        <Button
+                            style={home.button}
+                            onPress={() => {this.submit()}}
+                            title="START"
+                        />
+                    </View>          
+                </ImageBackground>
             )
     }
 }
---
+++ components/Style.js
@@ -0,0 +1,41 @@
+module.exports = {
+    home: {
+        image: {
+            flex: 1,
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            justifyContent: 'center'
+        },
+        view: {
+            height: '65%',
+            borderWidth: 1,
+            borderColor: 'black'
+        },
+        title: {
+            fontFamily: 'Montserrat-Light',
+            fontSize: 42,
+            color: 'white',
+            textAlign: 'center',
+            borderWidth: 1,
+            borderColor: 'red'
+        },
+        input: {
+            fontSize: 16,
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+            marginTop: '25%',
+            fontStyle: 'italic',
+            borderWidth: 1,
+            borderColor: 'red',
+            textAlign: 'center',
+        },
+        button: {
+            borderWidth: 1,
+            borderColor: 'blue',
+            marginTop: 10,
+            fontSize: 100
+        }
+    }
+}
